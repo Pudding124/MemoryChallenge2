05cdb182f61c3e6faa6590de8cfcc08621f28b4c
Ming Jen <surprised128@gmail.com>
2018-05-11 15:38:53 +0800

    Initial Commit

---
+++ MemoryChallenge2Tests/MemoryChallenge2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MemoryChallenge2Tests.swift
+//  MemoryChallenge2Tests
+//
+//  Created by 許銘仁 on 2018/5/11.
+//  Copyright © 2018年 Xumingjen. All rights reserved.
+//
+
+import XCTest
+@testable import MemoryChallenge2
+
+class MemoryChallenge2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
